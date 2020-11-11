e1b66d6e147118787695e9f2227ca7b57516f6da
taiga <taiga.similarities@gmail.com>
2020-11-12 01:14:34 +0900

    fixed feature/slideshow-component

--- src/redux/reducers/movieReducer.js
+++ src/redux/reducers/movieReducer.js
@@ -1,8 +1,8 @@
+/* eslint-disable default-param-last */
 const initialState = {
   list: []
 };
 
-// eslint-disable-next-line default-param-last
 export default (state = initialState, action) => {
   switch (action.type) {
     default:

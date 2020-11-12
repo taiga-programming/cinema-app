0561d5130eeb16c394e3b174746ada8ba4ba9d55
taiga <taiga.similarities@gmail.com>
2020-11-13 02:42:54 +0900

    fixed feature/slideshow-component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 
 import './App.scss';
 import store from './redux/store';
-import Header from './components/header/Header';
+import Header from './components/head/Header';
 import Main from './components/main/Main';
 
 const App = () => {

66adbb5b703bec215ded8acd6f9216e31b38ca2d
taiga <taiga.similarities@gmail.com>
2020-11-23 16:47:53 +0900

    fixed errors on feature/search-movies-functionality

--- src/services/movies.service.js
+++ src/services/movies.service.js
@@ -10,6 +10,6 @@ export const MOVIE_API_URL = async (type, page) => {
 };
 
 export const SEARCH_API_URL = async (query) => {
-  const response = await axios.get(`${REQUEST_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=${query}`);
+  const response = await axios.get(`${REQUEST_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${query}`);
   return response;
 };
